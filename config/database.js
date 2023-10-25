76735a421bd4c1d4ae85482ecbfcca27da5e77f2
Jihun Song <songjihun940409@gmail.com>
2023-10-25 14:29:26 +0900

    db 연결

--- config/database.js
+++ config/database.js
@@ -56,11 +56,11 @@ module.exports = ({ env }) => {
     postgres: {
       connection: {
         connectionString: env("DATABASE_URL"),
-        host: env("DATABASE_HOST", "localhost"),
+        host: env("DATABASE_HOST", "127.0.0.1"),
         port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapi"),
-        user: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
+        database: env("DATABASE_NAME", "petmily"),
+        user: env("DATABASE_USERNAME", "root"),
+        password: env("DATABASE_PASSWORD", "1234"),
         ssl: env.bool("DATABASE_SSL", false) && {
           key: env("DATABASE_SSL_KEY", undefined),
           cert: env("DATABASE_SSL_CERT", undefined),
