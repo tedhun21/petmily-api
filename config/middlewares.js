43b6cb117dd583fed369c874f4684a4f8b85faee
Jihun Song <songjihun940409@gmail.com>
2023-10-26 15:22:34 +0900

    cors 설정

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,18 @@
 module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  "strapi::security",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["https://petmily.shop"],
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
+    },
+  },
 ];
